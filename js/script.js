7a5a48ea71d60d9753bf297ec0669ca60085803d
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-19 19:25:53 -0300

    edit notes

--- js/script.js
+++ js/script.js
@@ -62,6 +62,12 @@ function createNote(id, content, fixed) {
   }
 
   // Element's Events
+  element.querySelector("textarea").addEventListener("keyup", (e) => {
+    const noteContent = e.target.value;
+
+    updateNote(id, noteContent);
+  });
+
   element.querySelector(".bi-pin").addEventListener("click", () => {
     toggleFixNote(id);
   });
@@ -112,6 +118,13 @@ function copyNote(id) {
   );
   notesContainer.appendChild(noteElement);
   notes.push(noteObject);
+  saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id === id)[0];
+  targetNote.content = newContent;
   saveNotes(notes);
 }
 
