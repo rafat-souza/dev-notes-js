a297a4122f8421c72f66e9b3c021efa716ecba5a
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-19 18:42:43 -0300

    fixed notes

--- js/script.js
+++ js/script.js
@@ -38,7 +38,27 @@ function createNote(id, content, fixed) {
   textarea.value = content;
   textarea.placeholder = "Adicione algum texto...";
   element.appendChild(textarea);
+  const pinIcon = document.createElement("i");
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+  element.appendChild(pinIcon);
+  if (fixed) {
+    element.classList.add("fixed");
+  }
+
+  // Element's Events
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
   return element;
+}
+
+function toggleFixNote(id) {
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  targetNote.fixed = !targetNote.fixed;
+  saveNotes(notes);
 }
 
 // Local Storage
