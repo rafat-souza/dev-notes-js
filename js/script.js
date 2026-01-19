76b6ad50b065d7a097a72b44a9345c1ca6ffb432
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-19 19:18:45 -0300

    deleting and duplicating notes

--- js/script.js
+++ js/script.js
@@ -44,9 +44,19 @@ function createNote(id, content, fixed) {
   textarea.value = content;
   textarea.placeholder = "Adicione algum texto...";
   element.appendChild(textarea);
+
   const pinIcon = document.createElement("i");
   pinIcon.classList.add(...["bi", "bi-pin"]);
   element.appendChild(pinIcon);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.classList.add(...["bi", "bi-x-lg"]);
+  element.appendChild(deleteIcon);
+
+  const duplicateIcon = document.createElement("i");
+  duplicateIcon.classList.add(...["bi", "bi-file-earmark-plus"]);
+  element.appendChild(duplicateIcon);
+
   if (fixed) {
     element.classList.add("fixed");
   }
@@ -55,6 +65,17 @@ function createNote(id, content, fixed) {
   element.querySelector(".bi-pin").addEventListener("click", () => {
     toggleFixNote(id);
   });
+
+  element.querySelector(".bi-x-lg").addEventListener("click", () => {
+    deleteNote(id, element);
+  });
+
+  element
+    .querySelector(".bi-file-earmark-plus")
+    .addEventListener("click", () => {
+      copyNote(id);
+    });
+
   return element;
 }
 
@@ -66,6 +87,32 @@ function toggleFixNote(id) {
   targetNote.fixed = !targetNote.fixed;
   saveNotes(notes);
   showNotes();
+}
+
+function deleteNote(id, element) {
+  const notes = getNotes().filter((note) => note.id !== id);
+
+  saveNotes(notes);
+
+  notesContainer.removeChild(element);
+}
+
+function copyNote(id) {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id === id)[0];
+  const noteObject = {
+    id: generateId(),
+    content: targetNote.content,
+    fixed: false,
+  };
+  const noteElement = createNote(
+    noteObject.id,
+    noteObject.content,
+    noteObject.fixed,
+  );
+  notesContainer.appendChild(noteElement);
+  notes.push(noteObject);
+  saveNotes(notes);
 }
 
 // Local Storage
