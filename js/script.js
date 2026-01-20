038fac6075cc6c5cb171f27bf277fa93f4df2fbc
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-20 00:13:34 -0300

    export CSV

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#export-notes");
 
 // Functions
 function showNotes() {
@@ -163,6 +164,27 @@ function searchNotes(search) {
   showNotes();
 }
 
+function exportData() {
+  const notes = getNotes();
+
+  const csvString = [
+    ["ID", "Content", "Fixed"],
+    ...notes.map((note) => [note.id, note.content, note.fixed]),
+  ]
+    .map((e) => e.join(","))
+    .join("\n");
+
+  const element = document.createElement("a");
+
+  element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+
+  element.target = "_blank";
+
+  element.download = "notes.csv";
+
+  element.click();
+}
+
 // Events
 addNoteBtn.addEventListener("click", () => addNote());
 
@@ -176,6 +198,10 @@ noteInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     addNote();
   }
+});
+
+exportBtn.addEventListener("click", () => {
+  exportData();
 });
 
 // Init
